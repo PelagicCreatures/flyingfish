32cc5a2c0f0ce512f53872e0a719672d42ca7ac8
Michael Rhodes <mrhodes@mediapolis.com>
2020-01-24 14:42:14 -0400

    .

--- dist/flyingfish.es.js
+++ dist/flyingfish.es.js
@@ -34,33 +34,33 @@ class FlyingFish extends Sargasso {
 	constructor (element, options = {}) {
 		super(element, {
 			watchViewport: true
-		})
-		this.triggered = false
+		});
+		this.triggered = false;
 	}
 
 	enterViewport () {
 		if (!this.triggered) {
-			this.triggered = true
+			this.triggered = true;
 
 			const offload = `onmessage = async (e) => {
 			  const response = await fetch(e.data.url)
 			  const blob = await response.blob()
 			  self.postMessage({ uid: e.data.uid, blob: blob })
-			}`
+			}`;
 
-			this.workerStart('FlyingFish', offload)
+			this.workerStart('FlyingFish', offload);
 
-			let imgUrl = this.element.getAttribute('data-src')
+			let imgUrl = this.element.getAttribute('data-src');
 
 			// not fully qualified...
 			if (!imgUrl.match(/^http/)) {
-				imgUrl = new URL(imgUrl, location.href).href
+				imgUrl = new URL(imgUrl, location.href).href;
 			}
 
 			// hand the url to the worker for loading
 			this.workerPostMessage('FlyingFish', {
 				url: imgUrl
-			})
+			});
 		}
 	}
 
@@ -68,33 +68,33 @@ class FlyingFish extends Sargasso {
 	workerOnMessage (id, data) {
 		if (id === 'FlyingFish') {
 			if (data.uid === this.uid) {
-				this.blobURL = URL.createObjectURL(data.blob)
+				this.blobURL = URL.createObjectURL(data.blob);
 				const frame = () => {
-					if (this.element.tagName === 'img') {
-						this.element.setAttribute('src', this.blobURL)
+					if (this.element.tagName === 'IMG') {
+						this.element.setAttribute('src', this.blobURL);
 					} else {
-						this.element.style.backgroundImage = 'url(' + this.blobURL + ')'
+						this.element.style.backgroundImage = 'url(' + this.blobURL + ')';
 					}
-					this.sleep() // We're done. That was easy.
-				}
-				this.queueFrame(frame)
+					this.sleep(); // We're done. That was easy.
+				};
+				this.queueFrame(frame);
 			}
 		}
-		super.workerOnMessage(id, data)
+		super.workerOnMessage(id, data);
 	}
 
 	destroy () {
 		if (this.blobURL) {
-			URL.revokeObjectURL(this.blobURL)
+			URL.revokeObjectURL(this.blobURL);
 		}
-		super.destroy()
+		super.destroy();
 	}
 }
 
-registerSargassoClass('FlyingFish', FlyingFish)
+registerSargassoClass('FlyingFish', FlyingFish);
 
 if (window) {
-	window.FlyingFish = FlyingFish
+	window.FlyingFish = FlyingFish;
 }
 
-export { FlyingFish }
+export { FlyingFish };
