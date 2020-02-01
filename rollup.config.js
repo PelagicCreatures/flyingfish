7d100f365c039424c07889015f121a899c81c015
Michael Rhodes <mrhodes@mediapolis.com>
2020-02-01 17:08:38 -0400

    iife for bundles

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,16 @@ import json from '@rollup/plugin-json'
 
 export default {
 	input: './index.js',
+	external: ['@PelagicCreatures/Sargasso'],
+
 	output: [{
-		format: 'es',
-		file: './dist/flyingfish.es.js'
+		format: 'iife',
+		name: 'PelagicCreatures.FlyingFish',
+		file: './dist/flyingfish.iife.js',
+		globals: {
+			'@PelagicCreatures/Sargasso': 'PelagicCreatures.Sargasso'
+		},
+		sourcemap: true
 	}],
 
 	plugins: [
